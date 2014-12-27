b5fe11a7654a58fe4dba6129ee70aab2778ad357
mr-fool <mr.law.jason@gmail.com>
2014-12-27 11:05:46 +0800

    added program.js

---
+++ Alcohol/program.js
@@ -0,0 +1,8 @@
+var alcohol = prompt("how much alcohol do you need to cope with javascript");
+
+while (alcohol !== "a lot") {
+	alert("You are wrong");
+	alcohol = prompt("how much alcohol do you need to cope with javascript");
+}
+alert("You are right alcohol is the only way to cope with javascript");
+
