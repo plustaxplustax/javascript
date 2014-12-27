e5ae7f72511a505c5401e07044ffd959ba8ce243
mr-fool <mr.law.jason@gmail.com>
2014-12-27 11:34:46 +0800

    panzerlied completed

---
+++ panzerlied/panzerlied.js
@@ -0,0 +1,31 @@
+document.writeln('Panzerlied Lyrics\n' +
+				'First stanza\n\n' +
+				'Ob\'s st\u00fcrmt oder schneit, ob die Sonne uns lacht,\n' +
+				'Der Tag gl\u00fchend heiÃŸ, oder eiskalt die Nacht,\n' +
+				'Bestaubt sind die Gesichter, doch froh ist unser Sinn, ja, unser Sinn.\n'+
+				'Es braust unser Panzer im Sturmwind dahin. (x2)\n\n' +
+				
+				'Second stanza\n' +
+				'Mit donnerndem Motor, geschwind wie der Blitz,\n' +
+				'Dem Feinde entgegen, im Panzer geschÃ¼tzt\n' +
+				'Voraus den Kameraden, Im Kampfe ganz allein, Ja ganz allein.n' +
+				'So stoÃŸen wir tief in die feindlichen Reih\'n! (x2)\n\n' +
+				
+				'Third stanza\n' +
+				'Wenn vor uns ein feindlicher Panzer erscheint,\n' +
+				'Wird Vollgas gegeben und ran an den Feind.\n' +
+				'Was gilt denn unser Leben fÃ¼r uns\'res Reiches Heer? Ja, Reiches Heer.\n'+
+				'FÃ¼r Deutschland zu sterben ist unsre hÃ¶chste Ehr\'. (x2)\n\n' +
+				
+				'Fourth stanza\n' +
+				'Mit Sperren und Minen hÃ¤lt der Gegner uns auf,\n' +
+				'Wir lachen darÃ¼ber und fahren nicht drauf.\n' +
+				'Und droh\'n vor uns GeschÃ¼tze Versteckt im gelben Sand, Ja, gelban Sand.\n'+
+				'Wir suchen uns Wege, die keiner sonst fand. (x2)\n\n' +
+				
+				'Fifth stanza\n' +
+				'Und lÃ¤ÃŸt uns im Stich einst das treulose GlÃ¼ck,\n' +
+				'Und kehren wir nicht mehr zur Heimat zurÃ¼ck,\n'+ 
+				'Trifft uns die Todeskugel, ruft uns das Schicksal ab, Ja, Schicksal ab.\n'+
+				'Dann wird unser Panzer ein ehernes Grab! (x2)'
+				);
