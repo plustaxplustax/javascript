94abe629510879ef648c3e47ddc1c0eb695d5078
mr-fool <mr.law.jason@gmail.com>
2014-12-27 11:28:41 +0800

    buzz.js added

---
+++ buzz/buzz.js
@@ -0,0 +1,15 @@
+for (var number = 1; number <= 100; number += 1) {
+	if (number %3 === 0 && number % 5 === 0) {
+		console.log("FizzBuzz");
+	}
+	else if (number %3 === 0) {
+		console.log("Fizz");
+	}
+	else if (number % 5 === 0) {
+		console.log("Buzz");
+	}
+	else {
+		console.log(number);
+	}
+	
+}
