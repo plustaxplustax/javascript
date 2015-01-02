cb7e1556fbf05838a65630d2998344153902fcd3
mr-fool <mr.law.jason@gmail.com>
2015-01-02 17:34:26 +0800

    Create reverse.js
    reversing array

---
+++ reverse/reverse.js
@@ -0,0 +1,10 @@
+function reverseArray(array){
+	var newArray = [];
+	for (var i = array.length, var j = 0; i > 0, j < array.length; i--,j++){
+		newArray[j]= array[i];
+	}
+	return newArray;
+}
+var array = {1,2,3,4};
+SyntaxError: missing ; before statement
+SyntaxError: missing variable name
