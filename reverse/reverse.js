b4f05219918e8c98ac686b8dfb579378fbf3ce55
mr-fool <mr.law.jason@gmail.com>
2015-01-02 22:33:12 +0800

    Update reverse.js

--- reverse/reverse.js
+++ reverse/reverse.js
@@ -8,3 +8,31 @@ function reverseArray(array){
 var array = [1,2,3];
 
 console.log(reverseArray(array));
+
+function reverseArrayInPlace(array){
+	var tmp = [];
+	for (var i = array.length -1, j = 0; i >= 0; i--,j++){
+		tmp[j]= array[i];
+	}
+	array = tmp.slice();
+	return array;
+}
+var array = [5,4,3,2,1];
+console.log(reverseArrayInPlace(array));
+
+/*better algorithm
+function reverseArray(array) {
+  var output = [];
+  for (var i = array.length - 1; i >= 0; i--)
+    output.push(array[i]);
+  return output;
+}
+
+function reverseArrayInPlace(array) {
+  for (var i = 0; i < Math.floor(array.length / 2); i++) {
+    var old = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = old;
+  }
+  return array;
+}*/
