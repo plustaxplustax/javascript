855af664b2c2819f0dc7133272d9d8966a0935fe
mr-fool <mr.law.jason@gmail.com>
2015-01-02 18:26:14 +0800

    Update reverse.js

--- reverse/reverse.js
+++ reverse/reverse.js
@@ -1,6 +1,6 @@
 function reverseArray(array){
 	var newArray = [];
-	for (var i = array.length, j = 0; i >= 0; i--,j++){
+	for (var i = array.length -1, j = 0; i >= 0; i--,j++){
 		newArray[j]= array[i];
 	}
 	return newArray;
@@ -8,4 +8,3 @@ function reverseArray(array){
 var array = [1,2,3];
 
 console.log(reverseArray(array));
-// my output [undefined, 3, 2, 1]
