0e1f895cbd638dd67c9c00d2524eca7590adceb3
mr-fool <mr-fool@users.noreply.github.com>
2015-01-17 08:23:50 -0700

    Create help.js

---
+++ help.js
@@ -0,0 +1,12 @@
+function gatherCorrelations(journal) {
+  var phis = {};
+  for (var entry = 0; entry < journal.length; entry++) {
+    var events = journal[entry].events;
+    for (var i = 0; i < events.length; i++) {
+      var event = events[i];
+      if (!(event in phis))
+        phis[event] = phi(tableFor(event, journal));
+    }
+  }
+  return phis;
+}
