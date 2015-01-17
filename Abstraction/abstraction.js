64258be552e9d16c65d4542e5b96bc49c96a6f93
mr-fool <mr-fool@users.noreply.github.com>
2015-01-17 07:49:31 -0700

    Create abstraction.js

---
+++ Abstraction/abstraction.js
@@ -0,0 +1,37 @@
+function abstraction(array, action){
+    for (var i =0; i < array.length; i++){
+        action(array[i]);
+    }
+}
+function mergeSort(array){
+    //argument checking
+    if (array.constructor !== Array ) {
+        return false;
+    }
+    //if the array is of length 0 or 1 there is no need to sort
+    if (array.length <= 1) {
+        return array;
+    }
+    var middle = Math.floor(array.length / 2);
+    var left = array.slice(0,middle);
+    var right = array.slice(middle);
+    return merge(mergeSort(left), mergeSort(right));
+}
+function merge(left, right){
+    var sortedArray = [];
+    var i;
+    var j;
+    for  (i = 0, j = 0; i < left.length && j < right.length;){
+        if (left[i] < right[j]) {
+            sortedArray.push(left[i]);
+            i++;
+        }
+        else {
+            sortedArray.push(right[j]);
+            j++;
+        }
+    }
+    return sortedArray.concat(left.slice(i)).concat(right.slice(j));
+}
+var testArray = [5,2,3];
+abstraction(mergeSort(testArray), console.log);
