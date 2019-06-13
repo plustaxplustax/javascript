bfff2c84ff0eb2ec92ea552c9ffa1b6e35808ea5
mr-fool <mr-fool@users.noreply.github.com>
2019-06-12 18:13:17 -0600

    Update repeating_number.js

--- repeating_number.js
+++ repeating_number.js
@@ -1,11 +1,18 @@
 let repeatNumbers = (data) => {
     let result = "";
+    
+    for(j = 0; j < data.length; j++) {
+        for (i = 0; i < data[j][1]; i++){
+            result += data[j][0];
+        }
+        if (j < data.length -1){
+            result += ", "
+        }
+        }
+        return result;
+    } 
 
-    for (i = 0; i < data.length){
 
-    }
-}
-
-console.log( repeatNumbers([1,10]) );
+console.log( repeatNumbers([ [1,10] ]) );
 console.log( repeatNumbers([  [1,2],[2,3] ]) );
 console.log( repeatNumbers( [ [10,4],[34,6], [92,2] ]) );
