2f424fad5ca42677cc8e6d2038940e2e7aab1ba0
mr-fool <mr-fool@users.noreply.github.com>
2019-06-12 23:50:04 -0600

    Update conditional_sum.js

--- conditional_sum.js
+++ conditional_sum.js
@@ -1,3 +1,22 @@
 let conditionalSum = (values, condition) => {
+    let result = 0;
 
-}
+    if (condition == "odd") {
+        remainder = 1;
+    }
+    else {
+        remainder = 0;
+    }
+
+    for (i=0; i < values.length; i++) {
+        if (values[i] % 2  === remainder) {
+            result += values[i];
+        }
+    }
+    return result;
+};
+ //testing case 
+console.log ( conditionalSum( [1,2,3,4,5], "even" ) ); //==> 6
+console.log ( conditionalSum( [1,2,3,4,5], "odd" ) ); //--> 9
+console.log ( conditionalSum( [13,88,12,44,99], "even" ) ); //--> 144
+console.log ( conditionalSum( [], "0dd" ) ); //--> 0
